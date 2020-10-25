3b1242df59ba7bad0ba6d399f06ee8c15d736bf0
JUshine <JUshine1209@gmail.com>
2020-10-25 16:09:35 +0900

    update

--- bookselector.py
+++ bookselector.py
@@ -1,8 +1,14 @@
 import json
+import random
 
 json_file = open('eng-metadata.json','r')
 json_data = json.load(json_file)
 json_file.close()
 
+booknum_list = []
 for data in json_data:
-    pass
+    booknum_list.append(data['txt_path'])
+
+random_booknum_list = random.sample(booknum_list,1000)
+random_booknum_list.sort()
+print(random_booknum_list)
