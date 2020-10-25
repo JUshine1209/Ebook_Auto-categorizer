96c5555787ac9e1310d450feb22530813257452d
JUshine <JUshine1209@gmail.com>
2020-10-25 17:19:18 +0900

    Update bookselector.py

--- bookselector.py
+++ bookselector.py
@@ -5,11 +5,10 @@ json_file = open('eng-metadata.json','r')
 json_data = json.load(json_file)
 json_file.close()
 
-booknum_list = []
+booknum_dict = {}
 for data in json_data:
-    booknum_list.append(data['txt_path'])
+    booknum_dict[data['txt_path']] = data['LoC_Class']
 
-random_booknum_list = random.sample(booknum_list,1000)
+random_booknum_list = random.sample(booknum_dict.keys(),1000)
 random_booknum_list.sort()
 print(random_booknum_list)
-print(len(random_booknum_list))
