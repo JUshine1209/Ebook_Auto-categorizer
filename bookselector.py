c199c8ff4434410a29cd470c5a4eddcbda7f3b58
JUshine <JUshine1209@gmail.com>
2020-10-26 02:05:11 +0900

    update

--- bookselector.py
+++ bookselector.py
@@ -1,5 +1,6 @@
 import json
 import random
+import shutil
 
 json_file = open('eng-metadata.json','r')
 json_data = json.load(json_file)
@@ -12,3 +13,17 @@ for data in json_data:
 random_booknum_list = random.sample(booknum_dict.keys(),1000)
 random_booknum_list.sort()
 print(random_booknum_list)
+
+first_write = True
+random_book_json = open('random_book.json','w')
+for book in random_booknum_list:
+    shutil.copy('gutenberg-dammit-files/'+book,'randombooks')
+    if first_write:
+        first_write = False
+    else :
+        random_book_json.write(',\n')
+    tmp_dict = {}
+    tmp_dict['LoC_Class'] = booknum_dict[book]
+    tmp_dict['txt_path'] = book
+    random_book_json.write(json.dumps(tmp_dict))
+
